d84a69617ea70d8d3d1a4f7cec839f06c1839438
faraz-Muhammad89 <pharazmuhammad@gmail.com>
2024-12-18 21:09:11 +0500

    java-assignment 1 completed

---
+++ app.js
@@ -0,0 +1,77 @@
+// Chapter No.1
+// Q No.1
+alert("HelloWorld") 
+
+
+
+//Q No.2
+alert("Error! Please enter valid password.")
+
+//Q. No.3
+alert("Welcome to JS Land... \n Happy Coding");
+
+//Q No.4
+alert("Welcome to JS Land");
+alert("Happy Coding");
+//Q No.5
+console.log("Hello... I can run JS through my web browser's console")
+
+
+//Chapter No.2
+
+//Q No.1
+var userName
+alert(userName)
+//Q No.2
+var myName= "muhammadFaraz"
+alert(myName);
+
+//Q No.3
+var message = "Hello World"
+alert(message);
+ 
+//Q No.4
+var stdName = "muhammad faraz"
+alert(stdName);
+ var stdAge = 15;
+ alert(stdAge)
+ var stdCourse = "certified Mobile Application Development";
+ alert(stdCourse)
+
+ //Q No.5
+ var foodName = ("Pizza\nPizz\nPiz\nPi\nP");
+alert(foodName);
+
+//Q No.6
+var email = ("pharazmuhammad@gmail.com")
+alert(`My email address is ${email}`)
+var book = "A smarter way to learn Javascript"
+alert(book);
+//Q Node.7
+book = "I am trying to learn from the book A smarter way to learn JavaScript "
+alert(book);
+
+//Q No.8
+document.write("Yah! I can write HTML content through JavaScript");
+
+//CHAPTER No.3 variables for numbers
+//Q No. 1
+var age = 15;
+alert(age);
+
+//Q No.2
+var user = "You have visited this site for 14 times"
+alert(user);
+
+
+//Q No.3
+var birthYear = 1998;
+document.write(`My birth year is  ${birthYear} <br/> Data type of my variable is number`);
+
+//Q No.4
+var online = "www.xyzClothing.com";
+var visitorsName ="john Doe";
+var qty = 5;
+var item = "T-shirts";
+document.write(`${visitorsName} ordered ${qty} ${item} on xyzClothing.com`)
+
